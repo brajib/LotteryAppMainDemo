8f3ef9a1498bcd761f766b09c2d57e718e8f0376
brajib <103416824+brajib@users.noreply.github.com>
2025-11-03 11:08:00 +0530

    Delete src directory

--- src/main/java/com/example/lotteryapp/LotteryRepo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.lotteryapp
-
-class LotteryRepo {
-    fun generateRandomNumber(count: Int): String {
-        val safeCount = count.coerceAtLeast(1)
-        val numbers = List(safeCount) { (1..42).random() }
-        return numbers.joinToString(", ")
-    }
-}
